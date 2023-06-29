09a64b0d02849491adc9c8fc2b6fe5fe4e05eab5
WillianMascarenhas <masca.will@gmail.com>
2023-06-29 19:01:59 -0300

    feat: User model/ beginning user serializer

--- users/models.py
+++ users/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    ...
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_employee = models.BooleanField(default=False)
+    updated_at = models.DateField(auto_now=True)
+
