75524e6bbaa18acf9973d46718890018d1f00dca
WillianMascarenhas <masca.will@gmail.com>
2023-07-03 15:52:51 -0300

    feat: Path user url/ user view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
+from rest_framework_simplejwt import views as jwt_views
+from .views import UserView
 
 urlpatterns = [
-    path(...),
+    path("users/", views.UserView.as_view()),
+    path("users/login/", jwt_views.TokenObtainPairView.as_view()),
 ]
