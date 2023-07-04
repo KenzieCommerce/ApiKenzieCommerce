3841d780b565d6bdb9d2ca6bb0e2dc0375429d5e
Gui2912 <dainize22@gmail.com>
2023-07-04 10:34:36 -0300

    feat: product and serializer

--- products/serializer.py
+++ products/serializer.py
@@ -9,4 +9,10 @@ class ProductSerializer(serializers.ModelSerializer):
         extra_kwargs = {"user": {"read_only": True}}
 
     def create(self, validated_data):
-        return super().create(**validated_data)
+        # user = validated_data.pop("user")
+        if validated_data.get("stock", 0) > 0:
+            validated_data["available"] = True
+        else:
+            validated_data["available"] = False
+
+        return Product.objects.create(**validated_data)
