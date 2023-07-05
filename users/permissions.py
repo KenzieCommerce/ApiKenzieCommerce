45d3a77229126a995b848ba6f54b0da7425737e3
Marcelo Meirelles <meirellesm@hotmail.com>
2023-07-05 16:35:53 -0300

    feat: finish user view

--- users/permissions.py
+++ users/permissions.py
@@ -2,14 +2,14 @@ from rest_framework import permissions
 from .models import User
 from rest_framework.views import View
 
+
 class IsAccountOwnerAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User):
         if request.user.is_superuser:
             return True
-        if request.method in permissions.SAFE_METHODS:
-            return True
         return request.user.is_authenticated and obj == request.user
-    
+
+
 class IsEmployee(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User):
         print(request.user.is_employee)
