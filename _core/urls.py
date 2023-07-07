140d3c8771acf6fc836b544e0559c168826c9ea9
sandraoliv <sandra.so219@gmail.com>
2023-07-07 15:11:39 -0300

    deploy settings

--- _core/urls.py
+++ _core/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
+    SpectacularRedocView
 )
 
 urlpatterns = [
@@ -29,4 +30,5 @@ urlpatterns = [
     path("api/", include("orders.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
