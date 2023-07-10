0fee4c3cfcb6ddae759767628815c5e3754643fa
WillianMascarenhas <masca.will@gmail.com>
2023-07-10 16:50:08 -0300

    fix: create product with negative stock

--- products/models.py
+++ products/models.py
@@ -4,12 +4,18 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=40)
-    stock = models.IntegerField()
+    stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     user = models.ForeignKey(
-        "users.User", on_delete=models.PROTECT, related_name="product"
+        "users.User",
+        on_delete=models.PROTECT,
+        related_name="product"
     )
 
-    order = models.ManyToManyField("orders.Order", related_name="products")
+    order = models.ManyToManyField(
+        "orders.Order",
+        related_name="products"
+    )
+
